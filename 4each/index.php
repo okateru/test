e330dbb879b866db901fc381b36d0f4c07607a5a
okateru <honnosukosi@gmail.com>
2019-10-23 13:49:40 +0900

    20191023

--- 4each/index.php
+++ 4each/index.php
@@ -17,11 +17,15 @@
 				<li>登録フォーム</li>
 				<li>問い合わせ</li>
 				<li>その他</li>
-			</ul>		
+			</ul>
+            <br>
+            <br>
+            <br>
+            <h1>プログラミングに役立つ掲示板</h1>
 		<div class="left">
 			<form method="post" action="insert.php" class="form">
 			<section>
-				<h2>プログラミングに役立つ掲示板</h2>
+				<h2>入力フォーム</h2>
 			</section>
 			<section>
 				<h3>ハンドルネーム</h3>
@@ -42,61 +46,49 @@
 		</div>
 		<div class="right">
 			<section>
-				<p class="title">人気の記事</p>
+				<b><p class="title1">人気の記事</p></b>
 			</section>
 			<section>
-				<br>
 				<p class="element1">PHPオススメ本</p>
-				<br>
 				<p class="element2">PHP　Myadminの使い方</p>
-				<br>
 				<p class="element3">今人気のエディタTop5</p>
-				<br>
 				<p class="element4">HTMLの基礎</p>
-				<br>
 			</section>	
 			<section>
-				<p class="title">オススメリンク</p>
+				<b><p class="title1">オススメリンク</p></b>
 			</section>
 			<section>
-				<br>
 				<p class="element1">インターノウス株式会社</p>
-				<br>
 				<p class="element2">XAMPPのダウンロード</p>
-				<br>
 				<p class="element3">Eclipseのダウンロード</p>
-				<br>
 				<p class="element4">Braketsのダウンロード</p>
-				<br>
 			</section>
 			<section>
-				<p class="title">カテゴリ</p>
+				<b><p class="title1">カテゴリ</p></b>
 			</section>
 			<section>
-				<br>
 				<p class="element1">HTML</p>
-				<br>
 				<p class="element2">PHP</p>
-				<br>
 				<p class="element3">MySQL</p>
-				<br>
 				<p class="element4">JavaScript</p>
-				<br>
 			</section>	
 		</div>
 			
 			<?php
 			mb_internal_encoding("utf8");
-			$pdo=new PDO("mysql:dbname=contactform;host=localhost;","root","mysql");
+			$pdo=new PDO("mysql:dbname=lesson01;host=localhost;","root","mysql");
 			$stmt=$pdo->query("select *from 4each");
+            ?>
+            <?php
 			while($row=$stmt->fetch()){
 				echo"<div class='kiji'>";
 				echo"<h3>".$row['title']."</h3>";
 				echo"<div class='contents'>";
 				echo $row['comments'];
 				echo"<div class='handlename'>posted by ".$row['handlename']."</div>";
-				"</div>";
-				"</div>";
+                echo"</div>";
+				echo"</div>";
+				
 			}
 		?>
 			
