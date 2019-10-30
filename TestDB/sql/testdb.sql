8c7b23481f58870c16cf810d8b741bdb4950c093
okateru <honnosukosi@gmail.com>
2019-10-30 17:45:44 +0900

    20191030

--- TestDB/sql/testdb.sql
+++ TestDB/sql/testdb.sql
@@ -1,13 +1,13 @@
-drop database if exists testdb;
-create database testdb;
-use testdb;
+drop database if exists testdb11;
+create database testdb11;
+use testdb11;
 
-create table test_table(
+create table test_table11(
 user_id int,
 user_name varchar(255),
 password varchar(255)
 );
 
-insert into test_table values(1,"taro","123");
-insert into test_table values(2,"jiro","456");
-insert into test_table values(3,"hanako","789");
+insert into test_table11 values(1,"taro","123");
+insert into test_table11 values(2,"jiro","456");
+insert into test_table11 values(3,"hanako","789");
